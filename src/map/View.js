d53ae5a1fedc5e537e2e02fc7567f1c2c61480af
Graeme Yeates <yeatesgraeme@gmail.com>
2015-03-01 23:51:25 -0500

    Simple menu

--- src/map/View.js
+++ src/map/View.js
@@ -1,7 +1,10 @@
 import L from 'leaflet';
 require('leaflet-hash');
+require('leaflet-panel-layers/dist/leaflet-panel-layers.src');
+
 import esri from 'esri-leaflet';
 import Marionette from 'backbone.marionette';
+import _ from 'lodash';
 
 var View = Marionette.ItemView.extend({
 	template: false,
@@ -10,11 +13,27 @@ var View = Marionette.ItemView.extend({
     	"update": "updateView"
   	},
 
+  	baseLayers: ['Streets', 'Topographic', 'NationalGeographic', 'Oceans', 'ImageryLabels'],
+
 	onShow() {
-		global.map = this.map = L.map(this.el);
-		this.baseLayer = esri.basemapLayer('Streets').addTo(this.map);
+		global.map = this;
+		this.map = L.map(this.el);
 		this.updateView();
 		L.hash(this.map);
+
+
+		this.baseLayers = _.map(this.baseLayers, (layer, index) => {
+			return {
+				// Put a space between words
+				name: layer.split(/(?=[A-Z][^A-Z])/).join(" "),
+				// Make a feature layer
+				layer: esri.basemapLayer(layer),
+				// Mark the first one active
+				active: !index
+			};
+		});
+		this.menuControl = new L.Control.PanelLayers(this.baseLayers, null, {collapsed: false});
+		this.map.addControl( this.menuControl );
 	},
 
 	updateView() {
